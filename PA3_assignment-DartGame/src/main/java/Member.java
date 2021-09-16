a0a279829025b1c480c2f0209ca50d55f737cd73
Hai Ngo <h.ngo@student.fontys.nl>
2021-09-17 01:05:19 +0200

    Create first test for requirement "A player score can be added to a member", which will fail

---
+++ PA3_assignment-DartGame/src/main/java/Member.java
@@ -0,0 +1,11 @@
+public class Member {
+
+    /**
+     *
+     * @param playerScore
+     * @should add player score to a member
+     */
+    public void addScore(PlayerScore playerScore){
+
+    }
+}
---
+++ PA3_assignment-DartGame/src/test/java/MemberTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberTest {
+
+    /**
+     * @verifies add player score to a member
+     * @see Member#addScore(PlayerScore)
+     */
+    @Test
+    public void addScore_shouldAddPlayerScoreToAMember() throws Exception {
+        //TODO auto-generated
+        Assertions.fail("Not yet implemented");
+    }
+}
