e0f9766daa7a348331706e8795a6bd342c0be5dd
Hai Ngo <h.ngo@student.fontys.nl>
2021-09-17 12:52:04 +0200

    Create test for requirement "A date for a game is set when the game is created", which will fail

---
+++ PA3_assignment-DartGame/src/main/java/Game.java
@@ -0,0 +1,13 @@
+/**
+ * Game is responsible for keeping track of whose turn it is to play and the player scores during a game.
+ * When a game is finished, the winning member is also added.
+ */
+public class Game {
+
+    /**
+     * @should set date when game is created
+     */
+    public Game(){
+
+    }
+}
---
+++ PA3_assignment-DartGame/src/test/java/GameTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    /**
+     * @verifies set date when game is created
+     * @see Game#Game()
+     */
+    @Test
+    public void Game_shouldSetDateWhenGameIsCreated() throws Exception {
+        //TODO auto-generated
+        Assertions.fail("Not yet implemented");
+    }
+}
