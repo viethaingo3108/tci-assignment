7847350321b23063ffbfd8d95d1812706bdae9c7
Hai Ngo <h.ngo@student.fontys.nl>
2021-09-17 13:03:16 +0200

    Create implementation belongs to test "A date for a game is set when the game is created"

--- PA3_assignment-DartGame/src/main/java/Game.java
+++ PA3_assignment-DartGame/src/main/java/Game.java
@@ -1,13 +1,22 @@
+import java.util.Date;
+
 /**
  * Game is responsible for keeping track of whose turn it is to play and the player scores during a game.
  * When a game is finished, the winning member is also added.
  */
 public class Game {
 
+    private Date date;
+
     /**
      * @should set date when game is created
+     * @param date
      */
-    public Game(){
+    public Game(Date date){
+        this.date = date;
+    }
 
+    public Date getDate() {
+        return date;
     }
 }
--- PA3_assignment-DartGame/src/test/java/GameTest.java
+++ PA3_assignment-DartGame/src/test/java/GameTest.java
@@ -1,11 +1,16 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
 
+    private static Date VALID_DATE = new Date();
+
     /**
      * @verifies set date when game is created
-     * @see Game#Game()
+     * @see Game#Game(Date)
      */
     @Test
     public void Game_shouldSetDateWhenGameIsCreated() throws Exception {
