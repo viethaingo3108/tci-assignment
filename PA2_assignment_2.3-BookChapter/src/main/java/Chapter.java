e506360afc8b4095ab8fc3eb0c6d7c78930e18aa
Hai Ngo <h.ngo@student.fontys.nl>
2021-09-21 12:53:46 +0200

    Create test for Chapter.java

--- PA2_assignment_2.3-BookChapter/src/main/java/Chapter.java
+++ PA2_assignment_2.3-BookChapter/src/main/java/Chapter.java
@@ -1,5 +1,26 @@
+import java.util.List;
+
 public class Chapter {
     private String chapterName;
     private int chapterNumber;
-    //private List<SubChapter> subChapters;
+    private Chapter parent;
+    private List<Chapter> subChapters;
+
+    /**
+     * Constructor for Chapter
+     * @param chapterName
+     * @param chapterNumber
+     * @param parent
+     * @param subChapters
+     * @should set correct parameters
+     * @should throw IllegalArgumentException when name is null
+     * @should throw IllegalArgumentException when chapter number is less than 0
+     * @should show that chapters with same name and number are logically equal
+     */
+    public Chapter(String chapterName, int chapterNumber, Chapter parent, List<Chapter> subChapters) {
+        this.chapterName = chapterName;
+        this.chapterNumber = chapterNumber;
+        this.parent = parent;
+        this.subChapters = subChapters;
+    }
 }
