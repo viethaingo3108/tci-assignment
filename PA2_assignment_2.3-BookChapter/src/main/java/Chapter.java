eaac88a5e6a04a222e41b97aeef8574853068ef6
Hai Ngo <h.ngo@student.fontys.nl>
2021-09-21 12:54:59 +0200

    Recreate test for Chapter.java

--- PA2_assignment_2.3-BookChapter/src/main/java/Chapter.java
+++ PA2_assignment_2.3-BookChapter/src/main/java/Chapter.java
@@ -11,16 +11,14 @@ public class Chapter {
      * @param chapterName
      * @param chapterNumber
      * @param parent
-     * @param subChapters
      * @should set correct parameters
      * @should throw IllegalArgumentException when name is null
      * @should throw IllegalArgumentException when chapter number is less than 0
      * @should show that chapters with same name and number are logically equal
      */
-    public Chapter(String chapterName, int chapterNumber, Chapter parent, List<Chapter> subChapters) {
+    public Chapter(String chapterName, int chapterNumber, Chapter parent) {
         this.chapterName = chapterName;
         this.chapterNumber = chapterNumber;
         this.parent = parent;
-        this.subChapters = subChapters;
     }
 }
--- PA2_assignment_2.3-BookChapter/src/test/java/ChapterTest.java
+++ PA2_assignment_2.3-BookChapter/src/test/java/ChapterTest.java
@@ -1,47 +1,5 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChapterTest {
 
-    /**
-     * @verifies set correct parameters
-     * @see Chapter#Chapter(String, int, Chapter, java.util.List)
-     */
-    @Test
-    public void Chapter_shouldSetCorrectParameters() throws Exception {
-        //TODO auto-generated
-        Assertions.fail("Not yet implemented");
-    }
-
-    /**
-     * @verifies throw IllegalArgumentException when name is null
-     * @see Chapter#Chapter(String, int, Chapter, java.util.List)
-     */
-    @Test
-    public void Chapter_shouldThrowIllegalArgumentExceptionWhenNameIsNull() throws Exception {
-        //TODO auto-generated
-        Assertions.fail("Not yet implemented");
-    }
-
-    /**
-     * @verifies throw IllegalArgumentException when chapter number is less than 0
-     * @see Chapter#Chapter(String, int, Chapter, java.util.List)
-     */
-    @Test
-    public void Chapter_shouldThrowIllegalArgumentExceptionWhenChapterNumberIsLessThan0() throws Exception {
-        //TODO auto-generated
-        Assertions.fail("Not yet implemented");
-    }
-
-    /**
-     * @verifies show that chapters with same name and number are logically equal
-     * @see Chapter#Chapter(String, int, Chapter, java.util.List)
-     */
-    @Test
-    public void Chapter_shouldShowThatChaptersWithSameNameAndNumberAreLogicallyEqual() throws Exception {
-        //TODO auto-generated
-        Assertions.fail("Not yet implemented");
-    }
 }
